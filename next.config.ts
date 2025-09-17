1bdf2f94a280ca8a30eb435fc71e94158d7de0f0
Neil Carribine <ncarribine@hotmail.co.uk>
2025-09-17 20:50:18 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
 	images: {
 		remotePatterns: [new URL("https://a.storyblok.com/**")],
 	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 };
 
 export default nextConfig;
